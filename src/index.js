90648a479112be6b34637def68bfe2babdf07b72
mbbous <mbbous@gmail.com>
2021-01-22 22:37:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ let catButton = document.getElementById('give-cat')
 catButton.addEventListener("click", evt => {
 
     let catDiv = document.getElementById('cat-pic')
-    fetch('https://api.thecatapi.com/v1/images/search')
+
+    fetch('https://api.thecatapi.com/v1/images/search?')
     .then(res => res.json())
     .then(cats => {
         cats.forEach(cat => {
